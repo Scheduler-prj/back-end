f63a9cc74e104650f07d1242c699572c09e3b706
choizeus02 <choizeus0212@gmail.com>
2024-11-30 13:33:53 +0900

    yml 파일 분리 / 테스트 제거

--- src/test/java/PlanQ/PlanQ/PlanQApplicationTests.java
+++ src/test/java/PlanQ/PlanQ/PlanQApplicationTests.java
@@ -1,13 +1,13 @@
-package PlanQ.PlanQ;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PlanQApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package PlanQ.PlanQ;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PlanQApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
